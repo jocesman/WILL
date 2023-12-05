8368cde9dbab6fd6bf16296d275b188a97f6cd1d
jocesman <jicespedes1976@gmail.com>
2023-12-05 17:54:55 -0600

    Este es mi homework 01

--- 01.js
+++ 01.js
@@ -5,12 +5,22 @@
   No comentar la funcion 
 */
 function soloNumeros(array) {
-  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings 
+  /// llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var nuevoArray = [];
+
+array.forEach(function(element) {
+  if (typeof element === 'number') {
+    nuevoArray.push(element);
+  }
+});
+
+return(nuevoArray);
 
 }
 
