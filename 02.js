1d67f530379d30a16bb910e060a48d0e7d439975
jocesman <jicespedes1976@gmail.com>
2023-12-06 08:35:39 -0600

    Este es mi Homework 02

--- 02.js
+++ 02.js
@@ -5,13 +5,23 @@
   No comentar la funcion 
 */
 function stringMasLarga(strings) {
-  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
-  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo 
+  // de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo 
+  //(Es decir el de mayor cantidad de caracteres)
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
-
   // Tu código aca
+  mayor = strings[0].length;
+  dato = strings[0];
+  strings.forEach(function(element){
+    if (mayor < element.length) {
+        dato = element;
+        mayor= element.length;
+    }
+  });
+  return dato;
 }
 
 // No modifiques nada debajo de esta linea //
