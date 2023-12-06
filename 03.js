a246aaa71d394f41ead7ee4970622405cd39d594
jocesman <jicespedes1976@gmail.com>
2023-12-06 12:16:44 -0600

    Este es mi Homework 03

--- 03.js
+++ 03.js
@@ -5,15 +5,31 @@
   No comentar la funcion 
 */
 function buscarAmigo(amigos, nombre) {
-  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
-  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
-  // recibe un string llamado 'nombre'.
-  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+  // La funcion llamada 'buscarAmigo' recibe como argumento un array 
+  // llamado 'amigos' que contiene
+  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'.
+  // También recibe un string llamado 'nombre'.
+  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre'
+  // recibido por argumento.
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+aparecio = false;
+encontrado = {};
+
+amigos.forEach(function (element){
+    if (element.nombre === nombre) {
+        encontrado = {nombre: element.nombre,
+                      edad: element.edad};
+        aparecio = true;
+    }
+})
+
+if (aparecio) return (encontrado);
+else return ('No existe ese nombre solicitado');
+
   
 }
 
