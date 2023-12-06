6f976880085aca6e4101a903e67dc514aab594e2
jocesman <jicespedes1976@gmail.com>
2023-12-06 13:58:22 -0600

    Este es mi Homework 05

--- 05.js
+++ 05.js
@@ -5,15 +5,22 @@
   No comentar la funcion 
 */
 function pluck(array, propiedad) {
-  // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' y el nombre de una
-  // propiedad.
-  // La función debe devolver un nuevo arreglo con solo los valores dentro de la propiedad recibida
+  // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' 
+  // y el nombre de una propiedad.
+  // La función debe devolver un nuevo arreglo con solo los valores dentro de la 
+  // propiedad recibida
   // Ej:
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') debería devolver ['TV LCD', 'Computadora']
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
+  var nuevoArreglo = [];
+
+  nuevoArreglo= array.map(function(objeto) {
+    return objeto[propiedad];
+  });
+  return nuevoArreglo;
 
 }
 
